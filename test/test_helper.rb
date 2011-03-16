438d5ceed3fca104fd5c1c1b625e1302415034d2
Charles Feduke <charles.feduke@gmail.com>
2011-03-16 14:10:39 -0400

    using selenium with ff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,11 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require 'selenium-webdriver'
+Selenium::WebDriver.for :firefox
+Capybara.default_driver = :selenium
 
 Rails.backtrace_cleaner.remove_silencers!
-Capybara.default_driver = :rack_test
 Translator.reload!
 
 class ActiveSupport::TestCase
@@ -16,5 +18,5 @@ class ActiveSupport::TestCase
   #removed by instruction
   #fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  self.use_transactional_fixtures = false
 end
